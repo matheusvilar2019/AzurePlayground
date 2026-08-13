8d42931e7e6494dff7dcf5566034d9f464c5cf3d
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2026-08-12 23:16:38 -0300

    Added AzureBlobStorage Main Controller

---
+++ src/AzurePlayground.Api/Controllers/DocumentController.cs
@@ -0,0 +1,67 @@
+﻿using AzurePlayground.Application.DTOs;
+using AzurePlayground.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzurePlayground.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DocumentController : ControllerBase
+    {
+        private readonly IDocumentService _documentService;
+
+        public DocumentController(IDocumentService documentService)
+        {
+            _documentService = documentService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DocumentDTO>>> GetDocuments()
+        {
+            var documents = await _documentService.GetDocuments();
+
+            return Ok(documents);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<DocumentDTO>> GetById(int id)
+        {
+            var document = await _documentService.GetById(id);
+
+            if (document == null)
+                return NotFound();
+
+            return Ok(document);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] DocumentDTO documentDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _documentService.Add(documentDTO);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update([FromBody] DocumentDTO documentDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _documentService.Update(documentDTO);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Remove(int id)
+        {
+            await _documentService.Remove(id);
+
+            return NoContent();
+        }
+    }
+}
--- src/AzurePlayground.Application/Mappings/DomainToDTOMappingProfile.cs
+++ src/AzurePlayground.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
 using AzurePlayground.Application.DTOs;
+using AzurePlayground.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
