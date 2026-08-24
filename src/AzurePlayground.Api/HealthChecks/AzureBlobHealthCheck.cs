db335934b081e8dc35d9ea5508ac657fc1d1dbda
matheusvilar2019 <43919709+matheusvilar2019@users.noreply.github.com>
2026-08-23 23:51:35 -0300

    Added Serilog and Basic HealthCheck

---
+++ src/AzurePlayground.Api/HealthChecks/AzureBlobHealthCheck.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AzurePlayground.Api.HealthChecks
+{
+    public class AzureBlobHealthCheck : IHealthCheck
+    {
+        private readonly BlobContainerClient _containerClient;
+
+        public AzureBlobHealthCheck(BlobContainerClient containerClient)
+        {
+            _containerClient = containerClient;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _containerClient.ExistsAsync(cancellationToken);
+
+                return HealthCheckResult.Healthy(
+                    "Azure Blob Storage is available.");
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy(
+                    "Azure Blob Storage is unavailable.",
+                    ex);
+            }
+        }
+    }
+}
--- src/AzurePlayground.Api/Program.cs
+++ src/AzurePlayground.Api/Program.cs
@@ -1,10 +1,29 @@
+using AzurePlayground.Api.HealthChecks;
 using AzurePlayground.Infra.IoC;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext();
+});
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
 // Add services to the container.
+builder.Services.AddHealthChecks()
+    .AddSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")!,
+        name: "sqlserver");
+
+builder.Services
+    .AddHealthChecks()
+    .AddCheck<AzureBlobHealthCheck>(
+        "azure_blob",
+        timeout: TimeSpan.FromSeconds(5));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,6 +31,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
